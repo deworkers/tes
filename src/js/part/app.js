1874b071071e4e4dd04c41d3a9f843421312c85d
Евгений Беляев <z-be@yandex.ru>
2017-10-23 16:03:12 +0500

    fix

--- src/js/part/app.js
+++ src/js/part/app.js
@@ -62,6 +62,27 @@ $(document).ready(function() {
                 return element.find('img');
             }
         }
+    });
+
+    $('.achiev-list').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        closeOnContentClick: false,
+        closeBtnInside: false,
+        mainClass: 'mfp-with-zoom mfp-img-mobile',
+        image: {
+            verticalFit: true
+        },
+        gallery: {
+            enabled: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300, // don't foget to change the duration also in CSS
+            opener: function(element) {
+                return element.find('img');
+            }
+        }
         
     });
 
